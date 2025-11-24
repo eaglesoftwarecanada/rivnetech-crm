a702c361d4abd8fc55134dffa5e5175af9bf8b31
adazhii <adajii@gmail.com>
2025-11-24 14:12:29 +0200

    prepearing to store creads in db

--- healthcheck/services.py
+++ healthcheck/services.py
@@ -8,8 +8,33 @@ from django.core.cache import cache
 def cache_key_for_alias(alias: str) -> str:
     return f"healthcheck:{alias}"
 
+# def run_mirror_check_for_alias(alias: str) -> dict:
+#     cfg = HealthDatabase.objects.filter(alias=alias).first()
+#     if not cfg:
+#         return {
+#             "alias": alias,
+#             "ok": False,
+#             "status": "not_configured",
+#             "checked_at": None,
+#             "message": f"Alias '{alias}' not found in HEALTHCHECK_DBS",
+#         }
+#
+#     checker = HealthCheckerSQLMirrorSync(
+#         db_alias=cfg.alias,
+#         api_url=cfg.api_url,
+#         api_login=cfg.api_login,
+#         api_pass=cfg.api_pass,
+#         sync_wait_seconds=300,
+#     )
+#
+#     result = checker.check_mirror_synced()
+#
+#     cache.set(cache_key_for_alias(alias), result, timeout=60 * 60)
+#
+#     return result
+
 def run_mirror_check_for_alias(alias: str) -> dict:
-    cfg = HealthDatabase.objects.filter(alias=alias).first()
+    cfg = settings.HEALTHCHECK_DBS.get(alias)
     if not cfg:
         return {
             "alias": alias,
@@ -20,11 +45,11 @@ def run_mirror_check_for_alias(alias: str) -> dict:
         }
 
     checker = HealthCheckerSQLMirrorSync(
-        db_alias=cfg.alias,
-        api_url=cfg.api_url,
-        api_login=cfg.api_login,
-        api_pass=cfg.api_pass,
-        sync_wait_seconds=300,
+        db_alias=cfg["db_alias"],
+        api_url=cfg["api_url"],
+        api_login=cfg["api_login"],
+        api_pass=cfg["api_pass"],
+        sync_wait_seconds=300
     )
 
     result = checker.check_mirror_synced()
@@ -32,6 +57,7 @@ def run_mirror_check_for_alias(alias: str) -> dict:
     cache.set(cache_key_for_alias(alias), result, timeout=60 * 60)
 
     return result
+
 
 def iter_health_db_aliases():
     return list(getattr(settings, "HEALTHCHECK_DBS", {}).keys())
